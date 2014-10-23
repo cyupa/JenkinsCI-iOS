dd6c51a785b4ba5c809b7968fef51bfe3567c1dd
Ciprian Redinciuc <cyupa_15@yahoo.com>
2014-10-23 11:05:08 +0300

    Add empty test.

---
+++ JenkinsCITests/EmptyTest.m
@@ -0,0 +1,40 @@
+//
+//  EmptyTest.m
+//  JenkinsCI
+//
+//  Created by Ciprian Redinciuc on 23/10/14.
+//  Copyright (c) 2014 Ciprian Redinciuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface EmptyTest : XCTestCase
+
+@end
+
+@implementation EmptyTest
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
